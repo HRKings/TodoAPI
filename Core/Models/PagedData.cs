0bee0c52433f671cc2ec060282925e85305b3781
Helton Reis <heltonkreis@gmail.com>
2021-03-10 14:18:54 -0300

    [misc]{db} Add initial migration

---
+++ Core/Models/PagedData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Models
+{
+	public struct PagedData<T>
+	{
+		public int TotalItems { get; set; }
+		public int PageSize { get; set; }
+		public IEnumerable<T> Value { get; set; }
+	}
+}
---
+++ Infrastructure/Database/Migrations/20210310171758_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "todos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    name = table.Column<string>(type: "character varying(80)", maxLength: 80, nullable: false),
+                    completed = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "todos");
+        }
+    }
+}
