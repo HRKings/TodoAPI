ee4e8b183c4bec9890d47c189bcf4d8d77578027
Helton Reis <hrkings@rhaizzing.com>
2021-03-10 14:12:29 -0300

    [feature] Add DTOs

---
+++ Core/Dtos/TodoUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Dtos
+{
+	public class TodoUpdateDto
+	{
+		public string Name { get; set; }
+		[Required]
+		public bool Completed { get; set; }
+	}
+}
