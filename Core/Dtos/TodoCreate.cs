f8a29de09038a114603b7ac97a519cf771b7af38
Helton Reis <heltonkreis@gmail.com>
2021-03-10 14:12:29 -0300

    [feature] Add DTOs

---
+++ Core/Dtos/TodoCreate.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Dtos
+{
+	public class TodoCreate
+	{
+		[Required]
+		public string Name { get; set; }
+		public bool Completed { get; set; }
+	}
+}
