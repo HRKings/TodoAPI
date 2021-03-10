594645bb76a450d0c6f8dfbb68af13eb49985583
Helton Reis <hrkings@rhaizzing.com>
2021-03-10 14:45:10 -0300

    [feature]{api} Add TODO controller

---
+++ API/Controllers/TodoController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Dtos;
+using Core.Models;
+using Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+	[Authorize]
+	[ApiController]
+	[Route("api/todos")]
+	[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+	[Produces("application/json")]
+	public class TodoController : ControllerBase
+	{
+		/// <summary>
+		///     Returns all the todos, can be paged using the page and pageSize query
+		/// </summary>
+		/// <response code="200">Returns all the todos</response>
+		/// <response code="400">If there is no todo on the database</response>
+		[HttpGet]
+		[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Todo>))]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<IActionResult> Get([FromServices] ITodoRepository repository, [FromQuery] int page = 0,
+			[FromQuery] int pageSize = 0)
+		{
+			if (page <= 0 || pageSize <= 0)
+			{
+				var result = await repository.GetAll();
+				if (result?.Count == 0)
+					return BadRequest("No TODOs were found");
+				
+				return Ok(result);
+			}
+
+			var pagedResult = await repository.GetPaged(page, pageSize);
+			if (pagedResult.TotalItems == 0)
+				return BadRequest("No TODOs were found");
+				
+			return Ok(pagedResult);
+		}
+		
+		/// <summary>
+		///     Returns a single todo by their id
+		/// </summary>
+		/// <response code="200">Returns the todo</response>
+		/// <response code="400">If there is not todo with this ID</response>
+		[HttpGet("{id}")]
+		[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Todo))]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<IActionResult> GetFromId([FromServices] ITodoRepository repository, [FromRoute] int id)
+		{
+			var result = await repository.GetFromId(id);
+
+			if (result == null)
+				return BadRequest($"The TODO {id} was not found.");
+
+			return Ok(result);
+		}
+		
+		/// <summary>
+		///     Creates a new todo
+		/// </summary>
+		/// <response code="200">Returns the created todo</response>
+		/// <response code="400">If there is an error</response>
+		[HttpPost]
+		[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Todo))]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public async Task<IActionResult> Create([FromServices] ITodoRepository repository,
+			[FromBody] TodoCreateDto todo)
+		{
+			try
+			{
+				var result = await repository.Create(todo);
+				return Ok(result);
+			}
+			catch (Exception e)
+			{
+				return BadRequest(e.Message);
+			}
+		}
+
+		/// <summary>
+		///     Updates a todo
+		/// </summary>
+		/// <response code="200">Returns the updated todo</response>
+		/// <response code="400">If there is an error</response>
+		[HttpPut("{id}")]
+		[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Todo))]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public async Task<IActionResult> Update([FromServices] ITodoRepository repository, [FromRoute] int id,
+			[FromBody] TodoUpdateDto todo)
+		{
+			try
+			{
+				var result = await repository.Update(id, todo);
+				if (result == null)
+					return BadRequest($"The id {id} was not found.");
+				
+				return Ok(result);
+			}
+			catch (Exception e)
+			{
+				return BadRequest(e.Message);
+			}
+		}
+
+		/// <summary>
+		///     Deletes a todo
+		/// </summary>
+		/// <response code="200">If the todo was deleted</response>
+		/// <response code="400">If there is an error</response>
+		[HttpDelete("{id}")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<IActionResult> Delete([FromServices] ITodoRepository repository, [FromRoute] int id)
+		{
+			try
+			{
+				await repository.Delete(id);
+				return Ok();
+			}
+			catch (Exception e)
+			{
+				return BadRequest(e.Message);
+			}
+		}
+	}
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using API.Repositories;
+using Infrastructure.Database.Context;
+using Infrastructure.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,12 +27,50 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoAPI", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "TODO API EntityFramework", Version = "v1"});
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme.<br/>
+                      Enter 'Bearer' [space] and then your token in the text input below.<br/>
+                      Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
+
+            services.AddDbContext<PostgresContext>(
+                options => options.UseNpgsql(
+                    Environment.GetEnvironmentVariable("DATABASE") ?? "Host=localhost;Database=challenge;Username=postgres;Password="));
+			
+            services.AddScoped(typeof(ITodoRepository), typeof(TodoRepository));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,7 +80,7 @@ namespace API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoAPI v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TODO API v1"));
             }
 
             app.UseRouting();
