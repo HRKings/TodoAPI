b1e2868c9ae48cd847a6b9f2cc839b45d7a18593
Helton Reis <hrkings@rhaizzing.com>
2021-03-10 13:58:23 -0300

    [misc]{db} Add database models and context

---
+++ Core/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿#nullable disable
+
+namespace Core.Models
+{
+    public partial class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Completed { get; set; }
+    }
+}
