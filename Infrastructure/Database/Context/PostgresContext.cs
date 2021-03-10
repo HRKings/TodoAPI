427ef9399056e439e4bb7b3420baa1951ba544f5
Helton Reis <heltonkreis@gmail.com>
2021-03-10 13:58:23 -0300

    [misc]{db} Add database models and context

---
+++ Infrastructure/Database/Context/PostgresContext.cs
@@ -0,0 +1,58 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace Infrastructure.Database.Context
+{
+    public partial class PostgresContext : DbContext
+    {
+        public PostgresContext()
+        {
+        }
+
+        public PostgresContext(DbContextOptions<PostgresContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=password");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "en_US.utf8");
+
+            modelBuilder.Entity<Todo>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("todos");
+
+                entity.Property(e => e.Completed).HasColumnName("completed");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("id")
+                    .UseSerialColumn();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(80)
+                    .HasColumnName("name");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
