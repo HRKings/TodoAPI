4974206ef50be4ba17dc333d428c7ca51ded3577
Helton Reis <heltonkreis@gmail.com>
2021-03-10 15:25:56 -0300

    [feature]{!!!/api} Add JWT authentication

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using API.Repositories;
+using Core.Utils;
+using Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+	[ApiController]
+	[Route("debug/auth")]
+	public class AuthController
+	{
+		public record UserAuth(string Email, string Password);
+		
+		[HttpPost]
+		[AllowAnonymous]
+		[Route("login")]
+		public async Task<IActionResult> Auth([FromServices] IUserRepository repository, [FromBody] UserAuth user)
+		{
+			try
+			{
+				var userExists = new UserRepository().GetByEmail(user.Email);
+
+				if (userExists == null)
+					return new BadRequestObjectResult(new {Message = "Email and/or password is invalid."});
+
+
+				if (userExists.Password != user.Password)
+					return new BadRequestObjectResult(new {Message = "Email and/or password is invalid."});
+				
+				string token = JwtUtils.GenerateToken(userExists);
+
+				return new OkObjectResult(new 
+				{
+					Token = token,
+					User = userExists
+				});
+			}
+			catch (Exception)
+			{
+				return new BadRequestObjectResult(new { Message = "Internal server error" });
+			}
+		}
+	}
+}
---
+++ API/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Enums;
+using Core.Utils;
+using Infrastructure.Interfaces;
+
+namespace API.Repositories
+{
+	public class UserRepository : IUserRepository
+	{
+		private readonly IList<JwtUtils.User> _users = new List<JwtUtils.User>
+		{
+
+			new
+			(
+				EnumUserType.Admin,
+				"admin@test.com",
+				"Hector",
+				"123"
+			),
+
+			new
+			(
+				EnumUserType.Moderator,
+				"mod@test.com",
+				"Maria",
+				"123"
+			),
+
+			new
+			(
+				EnumUserType.User,
+				"user@test.com",
+				"Jonas",
+				"123"
+			),
+		};
+
+		public IList<JwtUtils.User> GetAllUsers() => _users;
+
+		public JwtUtils.User GetByEmail(string email)
+			=> _users.FirstOrDefault(user 
+				=> string.Equals(user.Email, email, StringComparison.CurrentCultureIgnoreCase));
+	}
+}
