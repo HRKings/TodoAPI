8d181bf38e20da4044d9d8cb184292786ff003be
Helton Reis <hrkings@rhaizzing.com>
2021-03-10 14:23:52 -0300

    [feature] Create repository interface

---
+++ Infrastructure/Interfaces/ITodoRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Dtos;
+using Core.Models;
+
+namespace Infrastructure.Interfaces
+{
+	public interface ITodoRepository
+	{
+		public Task<List<Todo>> GetAll();
+
+		public Task<PagedData<Todo>> GetPaged(int page, int pageSize);
+
+		public ValueTask<Todo> GetFromId(int id);
+
+		public Task<Todo> Create(TodoCreateDto data);
+
+		public Task<Todo> Update(int id, TodoUpdateDto data);
+
+		public Task Delete(int id);
+	}
+}
